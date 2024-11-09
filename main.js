04d1bf0b3c3e7f2eaf954fe9978f5e2289566302
shakhzoda05 <shakhzoda05@gmail.com>
2024-11-10 01:03:36 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+"use strict";
+// 1-masala
+// let newValue:string | null = prompt("Qiymat kiriting")
+// let list:string[]=[]
+// function collectList(value:string | null):void {
+//     if (value){
+//         list.push(value)
+//     }
+// }
+// collectList(newValue)
+// console.log(list)
+// 2-masala
+// let numberList:number[]=[22,44,66,99]
+// function findMaxNumber(arr:number[]):number{
+//     let maxNum:number=arr[0]
+//     for(let num of arr){
+//         if(num>=maxNum){
+//             maxNum=num
+//         }
+//     }
+//     return maxNum
+// }
+// const result:number=findMaxNumber(numberList)
+// console.log(result)
+// 3-masala
+// let value:string | null =prompt("So'z kiriting...")
+// let list:string[]=["Salom","Nima gap"]
+// function findValue(newValue:string | null, arr:string[]):void{
+//     let count:number=0
+//     arr.forEach((item:string)=>{
+//         if(item==newValue){
+//             count++
+//         }
+//     })
+//     console.log(count> 0  ? true:false);
+// }
+// findValue(value,list)
+// 4-masala
+let numberList = [22, 15, 78, 88, 90];
